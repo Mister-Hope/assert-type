acdd07a975d1a3547b8582fd9278a8131795192f
Mr.Hope <mister-hope@outlook.com>
2022-05-13 00:18:31 +0800

    chore: release v1.2.1

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,18 @@ import pkg from "./package.json";
 import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
 
 export default [
   {
     input: "./src/index.ts",
     output: [{ file: pkg.main, format: "cjs", sourcemap: true }],
-    plugins: [json(), typescript()],
+    plugins: [json(), typescript(), terser()],
   },
   {
     input: "./src/index.ts",
     output: [{ file: pkg.module, format: "esm", sourcemap: true }],
-    plugins: [json(), typescript()],
+    plugins: [json(), typescript(), terser()],
   },
   {
     input: "./src/index.ts",
