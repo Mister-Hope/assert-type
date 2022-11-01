9a47c4e885ac3464595eef2cb33a6e68b8f67d3c
Mr.Hope <mister-hope@outlook.com>
2022-11-01 23:59:22 +0800

    chore: update files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-import pkg from "./package.json" assert { type: "json" };
 import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
@@ -7,17 +6,21 @@ import { terser } from "rollup-plugin-terser";
 export default [
   {
     input: "./src/index.ts",
-    output: [{ file: pkg.main, format: "cjs", sourcemap: true }],
+    output: [{ file: "./dist/index.cjs", format: "cjs", sourcemap: true }],
     plugins: [json(), typescript(), terser()],
   },
   {
     input: "./src/index.ts",
-    output: [{ file: pkg.module, format: "esm", sourcemap: true }],
+    output: [{ file: "./dist/index.mjs", format: "esm", sourcemap: true }],
     plugins: [json(), typescript(), terser()],
   },
   {
     input: "./src/index.ts",
-    output: [{ file: pkg.types, format: "esm", sourcemap: true }],
+    output: [
+      { file: "./dist/index.d.ts", format: "esm", sourcemap: true },
+      { file: "./dist/index.d.cts", format: "esm", sourcemap: true },
+      { file: "./dist/index.d.mts", format: "esm", sourcemap: true },
+    ],
     plugins: [dts()],
   },
 ];
